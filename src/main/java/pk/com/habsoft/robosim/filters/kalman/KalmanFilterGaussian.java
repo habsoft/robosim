c27249e3ce0a155c5f05ff6e2519963d440ad00b
Faisal Hameed <faisal.hameed@devfactory.com>
2016-05-11 22:06:47 +0500

    Fixing squid:S00117- Local variable and method parameter names should comply with a naming convention.

--- src/main/java/pk/com/habsoft/robosim/filters/kalman/KalmanFilterGaussian.java
+++ src/main/java/pk/com/habsoft/robosim/filters/kalman/KalmanFilterGaussian.java
@@ -17,9 +17,9 @@ public class KalmanFilterGaussian {
 	 * @return measurement update
 	 */
 	public static Mu_Sigma update(double mean1, double var1, double mean2, double var2) {
-		double new_mean = (mean1 * var2 + mean2 * var1) / (var1 + var2);
-		double new_var = 1 / (1 / var1 + 1 / var2);
-		return new Mu_Sigma(new_mean, new_var);
+		double newMean = (mean1 * var2 + mean2 * var1) / (var1 + var2);
+		double newVar = 1 / (1 / var1 + 1 / var2);
+		return new Mu_Sigma(newMean, newVar);
 	}
 
 	/**
@@ -31,17 +31,17 @@ public class KalmanFilterGaussian {
 	 * @return Motion/Prediction Update(Correction)
 	 */
 	public static Mu_Sigma predict(double mean1, double var1, double mean2, double var2) {
-		double new_mean = mean1 + mean2;
-		double new_var = var1 + var2;
-		return new Mu_Sigma(new_mean, new_var);
+		double newMean = mean1 + mean2;
+		double newVar = var1 + var2;
+		return new Mu_Sigma(newMean, newVar);
 	}
 
 	public static void main(String[] args) {
 
 		double[] measurements = { 5.0, 6.0, 7.0, 9.0, 10.0 };
-		double measurement_sig = 4.0;
+		double measurementSig = 4.0;
 		double[] motions = { 1.0, 1.0, 2.0, 1.0, 1.0 };
-		double motion_sig = 2.0;
+		double motionSig = 2.0;
 
 		// initial parameters
 		double mu = 0;
@@ -50,9 +50,9 @@ public class KalmanFilterGaussian {
 		Mu_Sigma newVal = new Mu_Sigma(mu, var);
 		for (int j = 0; j < measurements.length; j++) {
 			// System.out.println("Actual "+(newVal.mu));
-			newVal = update(newVal.mu, newVal.var, measurements[j], measurement_sig);
+			newVal = update(newVal.mu, newVal.var, measurements[j], measurementSig);
 			System.out.println("Update  " + newVal);
-			newVal = predict(newVal.mu, newVal.var, motions[j], motion_sig);
+			newVal = predict(newVal.mu, newVal.var, motions[j], motionSig);
 			System.out.println("Predict " + newVal);
 		}
 
